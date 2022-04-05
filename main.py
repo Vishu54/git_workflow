b10a6b62c53a1c850ea1597b8187bb26eb3fe98d
Vishwesh Keshri <vishu54@hotmail.com>
2022-04-05 22:02:40 +0530

    divide function added

--- main.py
+++ main.py
@@ -11,10 +11,15 @@ def multiply(a, b):
     return a * b
 
 
+def divide(a, b):
+    return a / b
+
+
 def main():
     add(1, 2)
     sub(1, 2)
     multiply(1, 2)
+    divide(1, 2)
 
 
 if __name__ == "__main__":
